674c494c6baafe48bce15c5a84c1b425b8816319
Charles Horn <charles.horn@gmail.com>
2019-12-01 00:09:34 +1300

    getting to the end of what can be golfed

--- nano-cassandra.sh
+++ nano-cassandra.sh
@@ -8,13 +8,16 @@ shuf -n1
 f(){
 e -B5 -A5 "$1" $2|tr -d '\n'|sed 's/ --/\n/g;s/^[^\.]*\([A-Z\.].\{18,\}\.\).*$/\1/'|h
 }
+x(){
+sed "$1"<<<$2
+}
 se(){
 S=''
-K=$(sed 's/ /\n/g;s/-//g'<<<$1|sort -n|uniq)
+K=$(x 's/ /\n/g;s/-//g' "$1"|sort -n|uniq)
 for k in $K; do 
 s=$(f $k $2)
-until [ -z "$k" ] || [ ! -z "$s" ];do
-k=$(sed 's/^.//'<<<$k)
+until [ -z $k ] || [ ! -z "$s" ];do
+k=$(x 's/^.//' $k)
 s=$(f $k $2)
 done
 S+=$s
@@ -24,24 +27,20 @@ echo $S
 re(){
 o=$1
 for w in $(e -o "\b\w[a-z]{$3,}\b"<<<$1|e -vi cass|shuf -n210);do
-o=$(sed "s/$w/$(e -o "\b\w[a-z]{$4,}\b" $2|h)/g"<<<$o)
+o=$(x "s/$w/$(e -o "\b\w[a-z]{$4,}\b" $2|h)/g" "$o")
 done
 echo $o
 }
 while read l;do
+g=''
 if [ $(wc -m<<<"$l") -gt 40 ];then
-l=$(sed "s/-\+/$(e -o "\w[a-z]{7,}" $2|h)/g"<<<$l)
-g=$(re "$(se "$l" $2 )" $1 4 4)
+l=$(x "s/-\+/$(e -o "\w[a-z]{7,}" $2|h)/g" "$l")
+g=$(re "$(se "$l" $2)" $1 4 4)
 l=$(re "$l" $2 7 7)
-sed 's/^\([^\.]*\).*$/\1./'<<<$l|tr -d '\n'
+x 's/^\([^\.]*\).*$/\1./' "$l"|tr -d '\n'
 if [[ $l =~ Phoe ]];then
 g=$(cut -d' ' -f-200<<<$g)
 fi
-sed "s/hrough/hro'/g;s/ful\([^l]\)/full\1/g;s/\band\b/\&/g;s/ien/ein/g"<<<$g
-sed 's/^[^\.]*\.\(.*\)$/\1/'<<<$l
-else
-sed 's/\(NOVEL\)/GENERATED \1/
-s/\(AUTHOR\)/\1MATON/
-s/by \(pe.*\b\) \(to\) \(Miss\)/without any \1 whatsoever \2 the \3es/'<<<$l
 fi
+x "s/\(NOVEL\)/GENERATED \1/;s/\(AUTHOR\)/\1MATON/;s/by \(pe.*\b\) to \(Miss\)/without any \1 whatsoever to the \2es/;s/^[^\.]*\.\(.*\)$/\1/;s/hrough/hro'/g;s/ful\([^l]\)/full\1/g;s/\band\b/\&/g;s/ien/ein/g" "$g$l"
 done<$1
