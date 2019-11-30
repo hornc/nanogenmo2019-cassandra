bf2456ba3e075ded085b5b6ad112ae470ee2b8ee
Charles Horn <charles.horn@gmail.com>
2019-11-30 23:38:17 +1300

    strip out more characters

--- nano-cassandra.sh
+++ nano-cassandra.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-h () {
+e(){
+egrep $@
+}
+h(){
 shuf -n1
 }
-f () {
-fgrep -B5 -A5 "$1" $2|tr -d '\n'|sed 's/ --/\n/g;s/^[^\.]*\([A-Z\.].\{18,\}\.\).*$/\1/'|h
+f(){
+e -B5 -A5 "$1" $2|tr -d '\n'|sed 's/ --/\n/g;s/^[^\.]*\([A-Z\.].\{18,\}\.\).*$/\1/'|h
 }
-se () {
+se(){
 S=''
 K=$(sed 's/ /\n/g;s/-//g'<<<$1|sort -n|uniq)
 for k in $K; do 
@@ -18,23 +21,23 @@ S+=$s
 done
 echo $S
 }
-re () {
+re(){
 o=$1
-for w in $(egrep -o "\b\w[a-z]{$3,}\b"<<<$1|grep -vi cassandra|shuf -n210);do
-o=$(sed "s/$w/$(egrep -o "\b\w[a-z]{$4,}\b" $2|h)/g"<<<$o)
+for w in $(e -o "\b\w[a-z]{$3,}\b"<<<$1|e -vi cass|shuf -n210);do
+o=$(sed "s/$w/$(e -o "\b\w[a-z]{$4,}\b" $2|h)/g"<<<$o)
 done
 echo $o
 }
 while read l;do
 if [ $(wc -m<<<"$l") -gt 40 ];then
-l=$(sed "s/----/$(egrep -o "\w[a-z]{7,}" $2|h)/g"<<<$l)
+l=$(sed "s/-\+/$(e -o "\w[a-z]{7,}" $2|h)/g"<<<$l)
 g=$(re "$(se "$l" $2 )" $1 4 4)
 l=$(re "$l" $2 7 7)
 sed 's/^\([^\.]*\).*$/\1./'<<<$l|tr -d '\n'
-if [[ $l =~ "Phoenix" ]];then
+if [[ $l =~ Phoe ]];then
 g=$(cut -d' ' -f-200<<<$g)
 fi
-sed "s/through/thro'/g;s/ful\([^l]\)/full\1/g;s/\band\b/\&/g;s/ien/ein/g"<<<$g
+sed "s/hrough/hro'/g;s/ful\([^l]\)/full\1/g;s/\band\b/\&/g;s/ien/ein/g"<<<$g
 sed 's/^[^\.]*\.\(.*\)$/\1/'<<<$l
 else
 sed 's/\(NOVEL\)/GENERATED \1/
