4f7bcd63441cd7907934371c860613721ef7f2e9
Charles Horn <charles.horn@gmail.com>
2019-12-01 03:55:29 +1300

    running out of ideas. Explore this idea further.

--- nano2.sh
+++ nano2.sh
@@ -1,36 +1,36 @@
-bash <(grep -v bash $0|sed 's/X/done\n/;s/Y/echo /;s/Q/$(egrep -o \"\\b\\w[a-z]{/;s/W\(.*\)W\(.\)/sed \"s\/\1\/g\"<<<$\2/') $1 $2;exit 7
+bash <(tail -n+2 $0|sed 's/X/done\n/;s/Y/echo /;s/Q/$(eJo \"\\b\\w[a-z]{/;s/W\(.*\)W\(.\)/sed \"s\/\1\/g\"Λ\2/;s/C/|shuf -n/;s/J/grep -/g;s/F/|tr -d \"\\n\"/;s/I/=$(/;s/Λ/<<<$/') $1 $2;exit 7
 f(){
-fgrep -B5 -A5 "$1" $2|tr -d '\n'|sed "s/ --/\n/g;s/^[^\.]*\([A-Z\.].\{18,\}\.\).*$/\1/"|shuf -n1
+JB5 -A5 "$1" $2F|sed "s/ --/\n/g;s/^[^\.]*\([A-Z\.].\{18,\}\.\).*$/\1/"C1
 }
 S(){
 S=''
-K=$(W /\n/g;s/-/W1|sort -n|uniq)
+KIW /\n/g;s/-/W1|sort -n|uniq)
 for k in $K;do
-s=$(f "$k" $2)
-until [ -z $k ] || [ ! -z "$s" ];do
-k=$(W^./Wk)
-s=$(f "$k" $2)
+sIf "$k" $2)
+until [ -z $k ]||[ ! -z "$s" ];do
+kIW^./Wk)
+sIf "$k" $2)
 XS+=$s
 XY$S
 }
 R(){
 o=$1
-for w in Q$3,}\b"<<<$1|grep -vi cassandra|shuf -n210);do
-o=$(W$w/Q$4,}\b" $2|shuf -n1)Wo)
+for w in Q$3,}\b"Λ1|Jvi cassaC210);do
+oIW$w/Q$4,}\b" $2C1)Wo)
 XY$o
 }
 while read l;do
-if [ $(wc -m <<<$l) -gt 40 ];then
-l=$(W-\+/Q7,}" $2|shuf -n1)Wl)
-g=$(R "$(S "$l" $2)" $1 4 4)
-l=$(R "$l" $2 7 7)
-W^\([^\.]*\).*$/\1.Wl|tr -d '\n'
-if [[ $l =~ "Phoenix" ]];then
-g=$(cut -d' ' -f-200<<<$g)
+if [ $(wc -mΛl) -gt 40 ];then
+lIW-\+/Q7,}" $2C1)Wl)
+gIR "$(S "$l" $2)" $1 4 4)
+lIR "$l" $2 7 7)
+W^\([^\.]*\).*$/\1.WlF
+if [[ $l =~ Phoe ]];then
+gIcut -d' ' -f-200Λg)
 fi
 Whrough/hro'/g;s/ful\([^l]\)/full\1/g;s/\band\b/\&/g;s/ien/einWg
 W^[^\.]*\.\(.*\)$/\1Wl
 else
-WNOVEL/GENERATED NOVEL/;s/AUTHOR/AUTHORMATON/;s/by \(pe.*\) to \(Miss\)/without any \1 whatsoever to the \2esWl
+WNOVEL/GENERATED \0/;s/AUTHOR/\0MATON/;s/by \(pe.*\) to \(Miss\)/without any \1 whatsoever to the \2esWl
 fi
 done<$1
