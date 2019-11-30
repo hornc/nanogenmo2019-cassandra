abfa82aaa8fb8fbc9b09b12469077acf2e738d72
Charles Horn <charles.horn@gmail.com>
2019-11-30 22:01:38 +1300

    truncate dedication, choose R. W. Chambers

--- cassandra.sh
+++ cassandra.sh
@@ -2,8 +2,12 @@
 
 seedtext=data/cassandra.txt
 
-sourceoneurl=https://archive.org/download/cbarchive_53241_onthemycologyandvisceralanatom1884/onthemycologyandvisceralanatom1884_djvu.txt
-sourceone=data/onthemycologyandvisceralanatom1884_djvu.txt
+#sourceoneurl=https://archive.org/download/cbarchive_53241_onthemycologyandvisceralanatom1884/onthemycologyandvisceralanatom1884_djvu.txt
+#sourceone=data/onthemycologyandvisceralanatom1884_djvu.txt
+
+sourceoneurl=https://archive.org/download/kinginyellow00chamrich/kinginyellow00chamrich_djvu.txt
+sourceone=data/kinginyellow00chamrich_djvu.txt
+
 
 # Get source text file, if not already present
 wget -nc -O $sourceone $sourceoneurl 
@@ -26,8 +30,8 @@ sentences () {
 # replace words from $1 with words in $2 file
 replace () {
 	out=$1
-        for w in $(egrep -o "\w[a-z]{$3,}" <<< $1| grep -vi cassandra | shuf -n210); do
-		out=$(sed "s/$w/$(egrep -o "\w[a-z]{$4,}" $2 | shuf -n1)/g" <<< $out)
+        for w in $(egrep -o "\b\w[a-z]{$3,}\b" <<< $1| grep -vi cassandra | shuf -n210); do
+		out=$(sed "s/$w/$(egrep -o "\b\w[a-z]{$4,}\b" $2 | shuf -n1)/g" <<< $out)
 	done
 	echo "$out"
 }
@@ -41,6 +45,10 @@ while read line; do
         genoutput=$(replace "$(sentences "$line" $sourceone)" data/cassandra.txt 4 4)
 	line=$(replace "$line" $sourceone 7 7)
 	sed 's/^\([^\.]*\).*$/\1./' <<< "$line" | tr -d '\n'
+	if [[ $line =~ "Phoenix" ]]; then
+		# truncate the dedication
+		genoutput=$(cut -d' ' -f-200 <<< "$genoutput")
+	fi
 	# Emulate the esteemed style of our original author:
 	sed "s/through/thro'/g;s/ful\([^l]\)/full\1/g;s/\band\b/\&/g;s/ien/ein/g" <<< "$genoutput"
         sed 's/^[^\.]*\.\(.*\)$/\1/' <<< "$line"
